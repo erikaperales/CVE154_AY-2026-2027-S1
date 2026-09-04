d240512225530e0f23adc12404c8a8c5294575cd
Christian Cahig <christian.cahig@outlook.com>
2026-09-04 14:14:20 +0800

    `act-01` : Add instructional notebook and template script

---
+++ materials/act-01/act-01.py
@@ -0,0 +1,12 @@
+"""
+CVE154 (A.Y. 2026-2027, S1)
+Template script for Programming Activity No. 01
+
+Prepared by Christian Cahig
+
+This file belongs to the repository
+https://github.com/christian-cahig/CVE154_AY-2026-2027-S1.
+"""
+
+if __name__ == "__main__":
+    pass
